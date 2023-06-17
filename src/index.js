b8bcba10dd7aeb608f5abf18c8cb9dfc7d6d2fc9
Ceren Şahin <cerennssahinn@gmail.com>
2023-06-17 23:26:21 +0300

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const submitBtn = document.querySelector("input[type=submit]");
+  submitBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    const task = document.querySelector("#new-task-description").value;
+    const taskList = document.querySelector("#tasks");
+    const newTask = document.createElement("textarea");
+    newTask.innerText = `${task}  `;
+    const deleteBtn = document.createElement("button");
+    deleteBtn.innerText = "X";
+    const toDoItemContainer = document.createElement("div");
+    toDoItemContainer.id = "to-do-item-container";
+    toDoItemContainer.appendChild(newTask);
+    toDoItemContainer.appendChild(deleteBtn);
+    taskList.appendChild(toDoItemContainer);
+
+    deleteBtn.addEventListener("click", (e) => {
+      e.target.parentNode.remove();
+    });
+
+    const form = document.querySelector("#create-task-form");
+    form.reset();
+    
+  })
+  
 });
